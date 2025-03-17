4bce3e3a5bfe45fd60587f8eddb16409a3b1a8af
Visual IQ <pvallasciani@visualiq.tech>
2025-03-17 16:29:01 -0300

    Add files via upload

--- VisualIQweb/VisualIQweb/settings.py
+++ VisualIQweb/VisualIQweb/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pvallasciani@visualiq.tech'  # Aquí debes colocar tu dirección de correo
+EMAIL_HOST_PASSWORD = 'exwy votq jslu bafn'  # Aquí debes colocar tu contraseña (o una contraseña de aplicación si usas autenticación en dos pasos)
--- VisualIQweb/Web/views.py
+++ VisualIQweb/Web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from django.core.mail import send_mail
 # Create your views here.
 
 def main(request):
@@ -17,4 +17,18 @@ def equipo(request):
     return render(request, 'equipo.html')
 
 def contacto(request):
+    if request.method == 'POST':
+        nombre = request.POST.get('nombre')
+        email = request.POST.get('email')
+        telefono = request.POST.get('telefono')
+        mensaje = request.POST.get('mensaje')
+        # Enviar correo con los datos del formulario
+        send_mail(
+            subject=f'Nuevo mensaje de {nombre} desde la web de Visual IQ',
+            message=f'Nombre: {nombre}\nEmail: {email}\nTeléfono: {telefono}\n\n\nMensaje: {mensaje}',
+            from_email='tuemail@example.com',  # Configura tu email en settings.py
+            recipient_list=['pvallasciani@visualiq.tech'],  # Cambia por tu email
+            fail_silently=False,
+        )
+        return HttpResponse("Mensaje enviado con éxito")
     return render(request, 'contacto.html')
